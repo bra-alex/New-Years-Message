c64423c2dd381007be1fcf551461e9f2e338f6c0
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-30 22:04:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ require('dotenv').config()
 
 const fs = require('fs')
 const path = require('path')
-const https = require('https')
+const http = require('http')
 
 const app = require('./app')
 const mongoConnect = require('./services/mongo')
 const { createAdmin } = require('./controllers/admin.controller')
 
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-}
+// const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+// }
 
-const httpsServer = https.createServer(options, app)
+// const httpsServer = https.createServer(options, app)
+const httpServer = http.createServer(app)
 
 async function startServer() {
     await mongoConnect()
     await createAdmin()
-    httpsServer.listen(process.env.PORT, () => console.log('Connected on port:', process.env.PORT))
+    httpServer.listen(process.env.PORT, () => console.log('Connected on port:', process.env.PORT))
 }
 
 startServer()
