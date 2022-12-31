d8716ddd4f8cc14e685fd3129270179a0793c2d5
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-31 02:03:05 +0000

    Initial Commit

---
+++ New Years Message Admin/New_Years_Message_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Years_Message_AdminApp.swift
+//  New Years Message Admin
+//
+//  Created by Don Bouncy on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct New_Years_Message_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
